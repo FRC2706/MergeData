1044f25a96682dd81615342d93e75d9b12be9daf
Ryanidk <ryan@ryanidk.com>
2024-02-10 14:07:35 -0500

    add github button

--- lib/screens/start.dart
+++ lib/screens/start.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:merge_data/screens/pit_scouting.dart';
 import 'package:merge_data/screens/match_scouting.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class StartPage extends StatefulWidget {
   const StartPage({super.key, required this.title, required this.year});
@@ -23,6 +25,15 @@ class _StartState extends State<StartPage> {
     );
   }
 
+  launchIssues() async {
+    Uri url = Uri.parse('https://github.com/FRC2706/MergeData/issues');
+    if (await launchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   void matchScouting() {
     Navigator.push(
       context,
@@ -41,6 +52,11 @@ class _StartState extends State<StartPage> {
           title:
               Text(widget.title, style: const TextStyle(color: Colors.black)),
         ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: launchIssues,
+            child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: SvgPicture.asset("assets/images/github.svg"))),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
