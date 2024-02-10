21b67ef5513461e935f01e074d4c3188ad3a4817
jwt2706 <jwt2home@gmail.com>
2024-02-10 13:19:10 -0500

    pit scouting seems to work

--- lib/screens/pit_scouting.dart
+++ lib/screens/pit_scouting.dart
@@ -1,48 +1,92 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class PitScoutingPage extends StatefulWidget {
-  const PitScoutingPage({Key? key, required this.title, required this.year})
-      : super(key: key);
+  PitScoutingPage({Key? key, required this.title, required this.year}) : super(key: key);
 
   final String title;
   final int year;
 
   @override
-  State<PitScoutingPage> createState() => _PitScoutingState();
+  _PitScoutingState createState() => _PitScoutingState();
 }
 
 class _PitScoutingState extends State<PitScoutingPage> {
-  TextEditingController _textEditingController = TextEditingController();
+  String? _selectedRadioValue = '';
+  Map<String, dynamic> formFields = {};
 
   @override
-  void dispose() {
-    _textEditingController.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    loadJson();
+  }
+
+  Future<void> loadJson() async {
+    final String response = await rootBundle.loadString("assets/pit/${widget.year}.json");
+    final data = await jsonDecode(response);
+    setState(() {
+      formFields = data;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title, style: const TextStyle(color: Colors.black)),
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text("Shep is cool"),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: _textEditingController,
-                decoration: InputDecoration(
-                  labelText: 'Enter a value',
-                ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: formFields['Pit']?.length ?? 0,
+        itemBuilder: (BuildContext context, int index) {
+          var field = formFields['Pit'][index];
+          if (field['type'] == 'number') {
+            return TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: field['name'],
               ),
-            ),
-          ],
-        ),
+              validator: (value) {
+                if (field['required'] && (value == null || value.isEmpty)) {
+                  return 'Please enter a value';
+                }
+                return null;
+              },
+            );
+          } else if (field['type'] == 'text') {
+            return TextFormField(
+              decoration: InputDecoration(
+                labelText: field['name'],
+              ),
+              validator: (value) {
+                if (field['required'] && (value == null || value.isEmpty)) {
+                  return 'Please enter a value';
+                }
+                return null;
+              },
+            );
+          } else if (field['type'] == 'radio') {
+            return Column(
+              children: field['choices'].map<Widget>((choice) {
+                return ListTile(
+                  title: Text(choice),
+                  leading: Radio<String>(
+                    value: choice,
+                    groupValue: _selectedRadioValue, // You need to manage this state
+                    onChanged: (String? value) {
+                      setState(() {
+                        _selectedRadioValue = value;
+                      });
+                    },
+                  ),
+                );
+              }).toList(),
+            );
+          } else {
+            return SizedBox.shrink(); // Return an empty widget for unsupported field types
+          }
+        },
       ),
     );
   }
