bcdbcf7f378234b9d1737cf33c73dcf993bc543a
jwt2706 <jwt2home@gmail.com>
2024-02-10 12:41:43 -0500

    json

--- lib/screens/pit_scouting.dart
+++ lib/screens/pit_scouting.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class PitScoutingPage extends StatefulWidget {
-  const PitScoutingPage({super.key, required this.title, required this.year});
+  const PitScoutingPage({Key? key, required this.title, required this.year})
+      : super(key: key);
 
   final String title;
   final int year;
@@ -11,19 +12,38 @@ class PitScoutingPage extends StatefulWidget {
 }
 
 class _PitScoutingState extends State<PitScoutingPage> {
+  TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title:
-              Text(widget.title, style: const TextStyle(color: Colors.black)),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title, style: const TextStyle(color: Colors.black)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text("Shep is cool"),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _textEditingController,
+                decoration: InputDecoration(
+                  labelText: 'Enter a value',
+                ),
+              ),
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[const Text("Shep is cool")],
-          ),
-        ));
+      ),
+    );
   }
 }
