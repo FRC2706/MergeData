45b85f84bf51554ffdb26b995ab0802a8497d3a3
jwt2706 <jwt2home@gmail.com>
2024-02-16 23:26:00 -0500

    qr gen btn + db btn

--- lib/screens/send_data.dart
+++ lib/screens/send_data.dart
@@ -3,33 +3,116 @@ import 'package:flutter/services.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'dart:convert';
 
-class SendData extends StatelessWidget {
+class SendData extends StatefulWidget {
   final Map<String, String> data;
 
   SendData({Key? key, required this.data}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    String dataString = jsonEncode(data);
+  _SendDataState createState() => _SendDataState();
+}
 
+class _SendDataState extends State<SendData> {
+  String? dataString;
+  bool showQR = false;
+  bool isCancelled = false;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Send Data'),
       ),
       body: ListView(
         children: [
-          ...data.entries.map((entry) {
+          ...widget.data.entries.map((entry) {
             return ListTile(
               title: Text(entry.key),
               subtitle: Text(entry.value),
             );
           }).toList(),
-          Center(
-            child: QrImageView(
-              data: dataString,
-              version: QrVersions.auto,
-              size: 200.0,
+          if (showQR)
+            Center(
+              child: QrImageView(
+                data: dataString!,
+                version: QrVersions.auto,
+                size: 200.0,
+              ),
             ),
+        ],
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          if (!showQR)
+            FloatingActionButton(
+              child: Icon(Icons.qr_code),
+              onPressed: () {
+                setState(() {
+                  dataString = jsonEncode(widget.data);
+                  showQR = true;
+                });
+              },
+            ),
+          SizedBox(width: 10), // Add some spacing between the buttons
+          FloatingActionButton(
+            child: Icon(Icons.send),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Confirmation'),
+                    content: Text('Make sure you have an internet connection to do this!'),
+                    actions: [
+                      TextButton(
+                        child: Text('SEND IT BABY!'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          showDialog(
+                            context: context,
+                            barrierDismissible: false,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    CircularProgressIndicator(),
+                                    Text("Shep is collecting the data..."),
+                                    Image.asset('assets/images/shep-loading.gif'),
+                                    TextButton(
+                                      child: Text('Cancel'),
+                                      onPressed: () {
+                                        isCancelled = true;
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                          Future.delayed(Duration(seconds: 3), () {
+                            if (!isCancelled) {
+                              Navigator.of(context).pop();
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('Shep collected the data, thanks scout! o7'))
+                              );
+                            }
+                          });
+                        },
+                      ),
+                      TextButton(
+                        child: Text('Cancel'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
           ),
         ],
       ),
