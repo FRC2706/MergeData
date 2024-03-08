81d7e8374bd84f148d9cc962a9d3c72bbcbd7bb5
jwt2706 <jwt2706@users.noreply.github.com>
2024-03-08 02:43:09 +0000

    deploy: 24c4908962c2452e933ef1d7fb7b0a111e6341c3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/assets/pit/2024.json": "a808c2b615505967a1d45cb651c5f574",
 "assets/assets/images/github.svg": "6088e31b17e2d7066c4ee055db90f8f0",
 "assets/assets/images/shep-loading.gif": "d053611e8a8e714d93747a1e3b1d0203",
-"assets/assets/games/2024.json": "fef152898032a7ce3bb97a87c5b7f830",
+"assets/assets/games/2024.json": "4d96b8d2137cbaaa2c91796c05bc4062",
 "assets/AssetManifest.bin": "2b8b613ecb17c7f3ace1b2adbe004320",
 "assets/AssetManifest.bin.json": "cae8585d96112eeaf06dc9212532e718",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
@@ -33,9 +33,9 @@ const RESOURCES = {"assets/assets/pit/2024.json": "a808c2b615505967a1d45cb651c5f
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.ico": "45caafee0f4096a5526e29a360baaba6",
 "manifest.json": "49d69b5666d182d198081780dc2d26c4",
-"index.html": "4082cc72b73e6233af403112197bf847",
-"/": "4082cc72b73e6233af403112197bf847",
-"main.dart.js": "618ef497b50473ea850b1b72512b6e73"};
+"index.html": "5c7dd0e9f40497255cf338217ec4a70b",
+"/": "5c7dd0e9f40497255cf338217ec4a70b",
+"main.dart.js": "f95a13f28060f34861e46f8c35032e77"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
